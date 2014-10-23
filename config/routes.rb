99ca7342016ce0ecfc94e0f59fc6d1e5ae2de279
figmentbml <beth.jaswa@gmail.com>
2014-10-23 08:59:31 -0600

    started work on links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root "pages#index"
+  get "/input_wine" => "pages#input", as: :input
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
