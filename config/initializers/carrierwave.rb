94b16c8c4adaffffd209f64463ce893a3ca12271
figmentbml <beth.jaswa@gmail.com>
2014-12-22 13:56:48 -0500

    commented out carrierwave test stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,13 +19,13 @@ CarrierWave.configure do |config|
     #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
 
   # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
+  # if Rails.env.test? || Rails.env.cucumber?
+  #   config.storage = :file
+  #   config.enable_processing = false
+  #   config.root = "#{Rails.root}/tmp"
+  # else
+  #   config.storage = :fog
+  # end
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
