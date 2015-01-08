c932897000971ef3def6584896fb5611b557b1a5
figmentbml <beth.jaswa@gmail.com>
2015-01-08 10:05:50 -0700

    yelp rake task to collect wineries was a success
    Needed to delete email validations

--- app/models/winery.rb
+++ app/models/winery.rb
@@ -7,7 +7,6 @@ class Winery < ActiveRecord::Base
   validates :state, presence: true
   validates :zip, presence: true
   validates :phone_number, presence: true
-  validates :email, presence: true
 
 
   def full_address
