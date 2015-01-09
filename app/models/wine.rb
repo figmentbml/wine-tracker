2584495b4d1f7764cfb99a2a4d43d39dac2d899c
figmentbml <beth.jaswa@gmail.com>
2015-01-09 14:36:25 -0700

    added varietals array to wine model & updated form

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,5 +1,97 @@
 class Wine < ActiveRecord::Base
 
   validates :grape_varietal, presence: true
+  validates :name, presence: true
+
+
+  def self.varietals
+    [
+      'Barbera',
+      'Cabernet Franc',
+      'Cabernet Sauvingnon',
+      'Chardonnay',
+      'Grenache',
+      'Gewurztraminer',
+      'Malbec',
+      'Marsanne',
+      'Merlot',
+      'Mourvedre',
+      'Muscat',
+      'Petite Sirah',
+      'Petite Verdot',
+      'Pinot Noir',
+      'Pinot Gris',
+      'Riesling',
+      'Roussanne',
+      'Sangiovese',
+      'Sauvignon Blanc',
+      'Semillon',
+      'Syrah',
+      'Shiraz',
+      'Tannat',
+      'Tempranillo',
+      'Viognier',
+      'Zinfandel',
+      'Blend',
+      'Other'
+    ]
+  end
+
+  def self.states
+    [
+      'Alabama',
+      'Alaska',
+      'Arizona',
+      'Arkansas',
+      'California',
+      'Colorado',
+      'Connecticut',
+      'Delaware',
+      'Flordia',
+      'Georgia',
+      'Hawaii',
+      'Idaho',
+      'Illinois',
+      'Indiana',
+      'Iowa',
+      'Kansas',
+      'Kentucky',
+      'Louisiana',
+      'Maine',
+      'Maryland',
+      'Massachusetts',
+      'Michigan',
+      'Minnesota',
+      'Mississippi',
+      'Missouri',
+      'Montana',
+      'Nebraska',
+      'Nevada',
+      'New Hampshire',
+      'New Jersey',
+      'New Mexico',
+      'New York',
+      'North Carolina',
+      'North Dakota',
+      'Ohio',
+      'Oklahoma',
+      'Oregon',
+      'Pennsylvania',
+      'Rhode Island',
+      'South Carolina',
+      'South Dakota',
+      'Tennessee',
+      'Texas',
+      'Utah',
+      'Vermont',
+      'Virginia',
+      'Washington',
+      'West Virgina',
+      'Wisconsin',
+      'Wyoming'
+    ]
+  end
+
+
 
 end
