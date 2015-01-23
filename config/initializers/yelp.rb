d1faf67b79e175190077a8e973c698a5c606b566
figmentbml <beth.jaswa@gmail.com>
2015-01-23 14:14:35 -0700

    updated yelp initalizer

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,7 +1,10 @@
 require 'yelp'
 Yelp.client.configure do |config|
-  config.consumer_key = ENV['YELP_CONSUMER_KEY']
-  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
-  config.token = ENV['YELP_TOKEN']
-  config.token_secret = ENV['YELP_TOKEN_SECRET']
+  if Rails.env.test?
+  else
+    config.consumer_key = ENV['YELP_CONSUMER_KEY']
+    config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
+    config.token = ENV['YELP_TOKEN']
+    config.token_secret = ENV['YELP_TOKEN_SECRET']
+  end
 end
