a55fa380b8d5c5e96d09b288d307e2134be13b7a
figmentbml <beth.jaswa@gmail.com>
2015-01-30 13:39:58 -0700

    deleted some comments in carrier wave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,8 @@
 CarrierWave.configure do |config|
-  # For testing, upload files to local `tmp` folder.
-  # if Rails.env.test? || Rails.env.cucumber?
-  #   config.storage = :file
-  #   config.enable_processing = false
-  #   config.root = "#{Rails.root}/tmp"
-  # else
-  #   config.storage = :fog
-  # end
   if Rails.env.test?
     config.storage = :file
     config.enable_processing = false
+  #   config.root = "#{Rails.root}/tmp"
   else
     config.storage = :fog
     config.fog_credentials = {
