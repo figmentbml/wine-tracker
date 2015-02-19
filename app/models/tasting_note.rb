8be3968569517b70d1ed91f0724e9c70c2ca02e8
figmentbml <beth.jaswa@gmail.com>
2015-02-19 10:36:21 -0700

    added wine rating validations

--- app/models/tasting_note.rb
+++ app/models/tasting_note.rb
@@ -3,4 +3,13 @@ class TastingNote < ActiveRecord::Base
   belongs_to :user
   belongs_to :wine
 
+  validates :wine_rating, presence: true
+  validate :rating_cannot_be_greater_than_5, on: [:create, :update]
+
+  def rating_cannot_be_greater_than_5
+    if wine_rating.present? && (wine_rating < 0 || wine_rating > 5)
+      errors.add(:wine_rating, "Ratings can only be 0-5")
+    end
+  end
+
 end
